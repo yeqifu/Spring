6c9292f18700f1656b6668003a6d40f59e9b74dc
yeqifu <1784525940@qq.com>
2020-03-26 23:11:38 +0800

    基于注解的声明式事务

---
+++ day04_eesy_05tx_anno/src/test/java/com/yeqifu/AccountTxTest.java
@@ -0,0 +1,24 @@
+package com.yeqifu;
+
+import com.yeqifu.service.IAccountService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @Author: 落亦-
+ * @Date: 2020/3/25 20:06
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:bean.xml")
+public class AccountTxTest {
+    @Autowired
+    private IAccountService as;
+
+    @Test
+    public  void testTransfer(){
+        as.transfer("ccc","bbb",1000);
+    }
+}
